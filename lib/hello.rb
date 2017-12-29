7a9806705a66af4a911d053e3b991d97532c9d26
tal shachar <tal.shachar@buzzfeed.com>
2017-12-29 00:37:48 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,10 +1,12 @@
 def hello_t(array)
   i = 0
-
+ 
   while i < array.length
-    yield array[i]
+    yield(array[i])
     i = i + 1
   end
+ 
+  array
 end
 # call your method here!
 hello_t(["Tim", "Tom", "Jim"]) do |name|
